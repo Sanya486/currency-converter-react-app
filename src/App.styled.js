dc4b2c78b04dcea5b24855e74bbfa5536406a5cf
Sanya486 <sanyok486@gmail.com>
2023-09-21 15:11:21 +0300

    change filenames

--- src/App.styled.js
+++ src/App.styled.js
@@ -1,5 +1,1 @@
-import styled from "styled-components";
 
-export const Wrap = styled.div`
-  padding: 10px;
-`;
--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Title} from './Page.styled'
+import {Title} from './PageTitle.styled'
 
 // import PropTypes from 'prop-types'
 
